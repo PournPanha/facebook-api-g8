768e9873458636750759e00e4fb9a8e127df2dfe
thary.oeun <thary.oeun@student.passerellernumeriques.org>
2024-06-11 11:05:32 +0700

    merge

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -44,7 +44,6 @@ class AuthController extends Controller
     public function logout(Request $request): JsonResponse
     {
         $request->user()->currentAccessToken()->delete();
-        
         return response()->json(['message' => 'Logout successful'], 200);
     }
     
